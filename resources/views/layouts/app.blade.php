7046a3b552b6840dd426f6ce45fe109ee675e258
Otomasi Admin <admin@otomasi.test>
2026-08-01 15:29:35 +0800

    Change favicon to match theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Otomasi Florist') }}</title>
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%2334d399' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M12 19l9 2-9-18-9 18 9-2zm0 0v-8'/%3E%3C/svg%3E">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
